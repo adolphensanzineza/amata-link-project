bf5a7bbad04b542163b5209cc04ee87b3631e402
serge740 <ishimweserge07@gmail.com>
2026-05-03 23:31:24 +0300

    aded

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ const PORT = process.env.PORT ? Number(process.env.PORT) : 5001;
 
 // Middleware
 app.use(cors({ 
-  origin: ['http://localhost:5173', 'http://localhost:5174'], 
+  origin: [process.env.CORS_URL,'http://localhost:5173', 'http://localhost:5174'], 
   credentials: true 
 }));
 app.use(express.json());
